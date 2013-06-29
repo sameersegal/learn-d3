f23c6fac8e320afa29e019302e064b75b4c4f29d
Sameer Segal <sameer@artoo.in>
2013-06-29 10:31:12 +0530

    Added all chapters' code

--- data/fixtures.js
+++ data/fixtures.js
@@ -11,6 +11,8 @@ App.Chapter.FIXTURES = [
 		"id": "my-first-graph",	
 		"title": "My First Graph",
 		"description": "D3 uses the top left corner as the origin. You need change your frame of refrence and think of it as you are drawing an inverted (y-axis) graph. We look at ```linear``` and ```ordinal``` scales.",
+		"seed": "1\n2\n3\n4",
+		"code": "function setup(data) {\n\tvar graph = d3.select(\"#graph\");\n\tvar g = graph\n\t\t\t   .append(\"svg\")\n\t\t\t   .append(\"g\");\n\treturn g;\n}\n",
 		"steps": [11,12,13,14,15]
 	},
 	// {
@@ -23,6 +25,8 @@ App.Chapter.FIXTURES = [
 		"id": "moving-scales",	
 		"title": "Moving Scales",
 		"description": "Now that you have your basics covered, lets try and combine all that we learnt with a constantly adapting graph -- even its scales adapt",
+		"seed": "1\n2\n3\n4",
+		"code": "function setup(data) {\n\tvar graph = d3.select(\"#graph\");\n\tvar g = graph\n\t\t\t   .append(\"svg\")\n\t\t\t   .append(\"g\");\n\treturn g;\n}\n",
 		"steps": [21,22,23]
 	},
 	// {
@@ -44,50 +48,52 @@ App.Step.FIXTURES = [
 		"id": 2,
 		"title": "Exit Data",
 		"text": "Remove a number from the data editor and see them disappear in the graph",
-		"code": null,
+		"code": "function setup(data) {\n\t// select the graph\n\tvar graph = d3.select(\"#graph\");\n\t// add add a container\n\tvar g = graph\n\t\t\t   .append(\"svg\")\n\t\t\t   .append(\"g\");\n\treturn g;\n}\n\nfunction draw(svg, data) {\n\tsvg.selectAll(\"circle\")\n\t .data(data)\n\t .exit()\n\t .attr(\"fill\",\"red\");\n}",
 		"data": "1\n2\n3\n4"
 	},
 	{
 		"id": 3,
 		"title": "Update Data",
 		"text": "Change data at each row and see the balls move",
-		"code": null,
+		"code": "function setup(data) {\n\t// select the graph\n\tvar graph = d3.select(\"#graph\");\n\t// add add a container\n\tvar g = graph\n\t\t\t   .append(\"svg\")\n\t\t\t   .append(\"g\");\n\treturn g;\n}\n\nfunction draw(svg, data) {\n\tsvg.selectAll(\"circle\")\n\t .data(data)\n\t .attr(\"cx\",function(d) { return d*50;})\n\t .attr(\"fill\",\"green\");\n}",
 		"data": "1\n2\n3\n4"
 	},
 	{
 		"id": 4,
 		"title": "Helping d3 distinguish data items",
 		"text": "Did you notice that d3 was only able to understand an item by its position in the array. Lets change that behaviour to value",
-		"code": null,
+		"code": "function setup(data) {\n\t// select the graph\n\tvar graph = d3.select(\"#graph\");\n\t// add add a container\n\tvar g = graph\n\t\t\t   .append(\"svg\")\n\t\t\t   .append(\"g\");\n\treturn g;\n}\n\nfunction draw(svg, data) {\n\tvar circles = svg.selectAll(\"circle\")\n\t .data(data, function(d,i) { return d;});\n\n\tcircles\n\t .enter()\n\t .append(\"circle\")\n\t .attr(\"cx\",function(d) { return d*50;})\n\t .attr(\"cy\",function(d) { return d*50;})\n\t .attr(\"r\",10)\n\t .attr(\"fill\",\"steelblue\");\n\n\tcircles\n\t .exit()\n\t .attr(\"fill\",\"red\");\n\n\tcircles\n\t .data(data)\n\t .attr(\"cx\",function(d) { return d*50;})\n\t .attr(\"fill\",\"green\");\n}",
 		"data": "1\n2\n3\n4"
 	},
+
+
 
 
 	{
 		"id": 11,
 		"title": "Scales: Linear",
 		"text": "Lets see how linear scale works",
-		"code": null,
+		"code": "function setup(data) {\n\tvar graph = d3.select(\"#graph\");\n\tvar g = graph\n\t\t\t   .append(\"svg\")\n\t\t\t   .append(\"g\");\n\treturn g;\n}\n\nfunction draw(svg, data) {\n\n\tvar y = d3.scale.linear().range([0,400]);\n\ty.domain(d3.extent(data));\n\n\tsvg.selectAll(\"rect\")\n\t .data(data)\n\t .enter()\n\t .append(\"rect\")\n\t .attr(\"x\",function(d,i) { return i*50;})\n\t .attr(\"height\",y)\n\t .attr(\"width\",30)\n\t .attr(\"fill\",\"steelblue\");\n}",
 		"data": "1\n2\n3\n4"
 	},
 	{
 		"id": 12,
 		"title": "Right way up",
 		"text": "Correct the graph so that it starts looking like a normal bar graph",
-		"code": null,
+		"code": "function setup(data) {\n\tvar graph = d3.select(\"#graph\");\n\tvar g = graph\n\t\t\t   .append(\"svg\")\n\t\t\t   .append(\"g\");\n\treturn g;\n}\n\nfunction draw(svg, data) {\n\n\tvar y = d3.scale.linear().range([0,400]);\n\ty.domain(d3.extent(data).reverse());\n\n\tvar rects = svg.selectAll(\"rect\")\n\t .data(data);\n\n\trects\n     .enter()\n\t .append(\"rect\")\n\t .attr(\"x\",function(d,i) { return i * 50; })\n\t .attr(\"width\",30)\n\t .attr(\"fill\",\"steelblue\");\n\n\trects\n\t .attr(\"y\",y)\n\t .attr(\"height\",function(d) { return 400 - y(d);});\t \n\n\trects\n\t .exit()\n\t .remove();\n}",
 		"data": "1\n2\n3\n4"
 	},
 	{
 		"id": 13,
 		"title": "Scales: Ordinal",
 		"text": "Lets see how ordinal scale works",
-		"code": null,
+		"code": "function setup(data) {\n\tvar graph = d3.select(\"#graph\");\n\tvar g = graph\n\t\t\t   .append(\"svg\")\n\t\t\t   .append(\"g\");\n\treturn g;\n}\n\nfunction draw(svg, data) {\n\n\tvar y = d3.scale.linear().range([0,400]);\n\ty.domain(d3.extent(data).reverse());\n\n\tvar rects = svg.selectAll(\"rect\")\n\t .data(data);\n\n\trects\n     .enter()\n\t .append(\"rect\")\n\t .attr(\"x\",function(d,i) { return i * 50; })\n\t .attr(\"width\",30)\n\t .attr(\"fill\",\"steelblue\");\n\n\trects\n\t .attr(\"y\",y)\n\t .attr(\"height\",function(d) { return 400 - y(d);});\t \n\n\trects\n\t .exit()\n\t .remove();\n}",
 		"data": "1\n2\n3\n4"
 	},
 	{
 		"id": 14,
-		"title": "Adding a scale",
-		"text": "To create adequate space for the axes, we translate the 'g' container",
+		"title": "Adding axes",
+		"text": "Read up on [Axes](https://github.com/mbostock/d3/wiki/SVG-Axes) and lets add x & y axes",
 		"code": null,
 		"data": "1\n2\n3\n4"
 	},
@@ -104,20 +110,20 @@ App.Step.FIXTURES = [
 		"id": 21,
 		"title": "Grow animaiton",
 		"text": "To grow, we force the graph to go from 0 to the current value using hte scale ",
-		"code": null,
+		"code": "function setup(data) {\n\tvar graph = d3.select(\"#graph\");\n\tvar g = graph\n\t\t\t   .append(\"svg\")\n\t\t\t   .append(\"g\");\n\treturn g;\n}\n\nfunction draw(svg, data) {\n\n\tvar y = d3.scale.linear().range([0,400]);\n\ty.domain(d3.extent(data).reverse());\n\n\tvar rects = svg.selectAll(\"rect\")\n\t .data(data);\n\n\trects\n     .enter()\n\t .append(\"rect\")\n\t .attr(\"x\",function(d,i) { return i * 50; })\n\t .attr(\"width\",30)\n\t .attr(\"fill\",\"steelblue\");\n\n\trects\n\t .transition()\n\t .duration(1000) \n\t .attr(\"y\",y)\n\t .attr(\"height\",function(d) { return 400 - y(d);});\t \n\n\trects\n\t .exit()\n\t .remove();\n}",
 		"data": "1\n2\n3\n4"
 	},
 	{
 		"id": 22,
 		"title": "Mouse events",
 		"text": "Add a mouseover and mouseout event",
-		"code": null,
+		"code": "function setup(data) {\n\tvar graph = d3.select(\"#graph\");\n\tvar g = graph\n\t\t\t   .append(\"svg\")\n\t\t\t   .append(\"g\");\n\treturn g;\n}\n\nfunction draw(svg, data) {\n\n\tvar y = d3.scale.linear().range([0,400]);\n\ty.domain(d3.extent(data).reverse());\n\n\tvar rects = svg.selectAll(\"rect\")\n\t .data(data);\n\n\trects\n     .enter()\n\t .append(\"rect\")\n\t .attr(\"x\",function(d,i) { return i * 50; })\n\t .attr(\"width\",30)\n\t .attr(\"fill\",\"steelblue\")\n\t .on(\"mouseover\",function(){\n\t \td3.select(this).attr('fill', 'red');\n\t })\n\t .on(\"mouseout\",function(){\n\t \td3.select(this).attr('fill', 'steelblue');\n\t });\n\n\trects\n\t .transition()\n\t .duration(1000) \n\t .attr(\"y\",y)\n\t .attr(\"height\",function(d) { return 400 - y(d);});\t \n\n\trects\n\t .exit()\n\t .remove();\n}\n",
 		"data": "1\n2\n3\n4"
 	},
 	{
 		"id": 23,
 		"title": "Moving scales",
-		"text": "Detect and add animation to your graphics to make them even more intuitive ",
+		"text": "Now lets make everything dynamic including data, axes, etc",
 		"code": null,
 		"data": "1\n2\n3\n4"
 	}
